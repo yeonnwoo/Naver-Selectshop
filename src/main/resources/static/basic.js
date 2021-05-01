725e2fe10844f7505a27fe1f6df68478a86351a0
YeonwooKim <50096655+Yeonwoo-Kim@users.noreply.github.com>
2021-05-01 16:11:46 +0900

    Update basic.js

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -1,7 +1,6 @@
 let targetId;
 
 $(document).ready(function () {
-    // id 가 query 인 녀석 위에서 엔터를 누르면 execSearch() 함수를 실행하라는 뜻입니다.
     $('#query').on('keypress', function (e) {
         if (e.key == 'Enter') {
             execSearch();
@@ -37,26 +36,16 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-///// 여기 아래에서부터 코드를 작성합니다. ////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-
+//검색어 입력 받아서 HTML로 만들고 붙이기
 function execSearch() {
-    /**
-     * 검색어 input id: query
-     * 검색결과 목록: #search-result-box
-     * 검색결과 HTML 만드는 함수: addHTML
-     */
-        // 1. 검색창의 입력값을 가져온다.
+   
     let query = $('#query').val();
 
-    // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
     if (query == '') {
         alert('검색어를 입력해주세요');
         $('#query').focus();
         return;
     }
-    // 3. GET /api/search?query=${query} 요청
     $.ajax({
         type: 'GET',
         url: `/api/search?query=${query}`,
@@ -73,12 +62,8 @@ function execSearch() {
 
 }
 
+//각각의 HTML 생성
 function addHTML(itemDto) {
-    /**
-     * class="search-itemDto" 인 녀석에서
-     * image, title, lprice, addProduct 활용하기
-     * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'
-     */
     return `<div class="search-itemDto">
         <div class="search-itemDto-left">
             <img src="${itemDto.image}" alt="">
@@ -96,35 +81,21 @@ function addHTML(itemDto) {
     </div>`
 }
 
+//관심목록에 추가
 function addProduct(itemDto) {
-    /**
-     * modal 뜨게 하는 법: $('#container').addClass('active');
-     * data를 ajax로 전달할 때는 두 가지가 매우 중요
-     * 1. contentType: "application/json",
-     * 2. data: JSON.stringify(itemDto),
-     */
-    // 1. POST /api/products 에 관심 상품 생성 요청
     $.ajax({
         type: "POST",
         url: '/api/products',
         contentType: "application/json",
         data: JSON.stringify(itemDto),
         success: function (response) {
-            // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (숙제로 myprice 설정하기 위함)
             $('#container').addClass('active');
-            targetId = response.id;
+            targetId = response.id; 
         }
     })
 }
 
 function showProduct() {
-    /**
-     * 관심상품 목록: #product-container
-     * 검색결과 목록: #search-result-box
-     * 관심상품 HTML 만드는 함수: addProductItem
-     */
-
-    // 1. GET /api/products 요청
     $.ajax({
         type: 'GET',
         url: '/api/products',
@@ -132,7 +103,7 @@ function showProduct() {
             // 2. 관심상품 목록, 검색결과 목록 비우기
             $('#product-container').empty();
             $('#search-result-box').empty();
-            // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+            // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기
             for (let i = 0; i < response.length; i++) {
                 let product = response[i];
                 let tempHtml = addProductItem(product);
@@ -143,7 +114,6 @@ function showProduct() {
 }
 
 function addProductItem(product) {
-    // link, image, title, lprice, myprice 변수 활용하기
     return `<div class="product-card" onclick="window.location.href='${product.link}'">
                 <div class="card-header">
                     <img src="${product.image}"
@@ -164,37 +134,23 @@ function addProductItem(product) {
 }
 
 function setMyprice() {
-    /**
-     * 숙제! myprice 값 설정하기.
-     * 1. id가 myprice 인 input 태그에서 값을 가져온다.
-     * 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
-     * 3. PUT /api/product/${targetId} 에 data를 전달한다.
-     *    주의) contentType: "application/json",
-     *         data: JSON.stringify({myprice: myprice}),
-     *         빠뜨리지 말 것!
-     * 4. 모달을 종료한다. $('#container').removeClass('active');
-     * 5, 성공적으로 등록되었음을 알리는 alert를 띄운다.
-     * 6. 창을 새로고침한다. window.location.reload();
-     */
-        // 1. id가 myprice 인 input 태그에서 값을 가져온다.
+
     let myprice = $('#myprice').val();
-    // 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
+    //예외처리
     if (myprice == '') {
         alert('올바른 가격을 입력해주세요');
         return;
     }
-    // 3. PUT /api/product/${targetId} 에 data를 전달한다.
+
     $.ajax({
         type: "PUT",
         url: `/api/products/${targetId}`,
         contentType: "application/json",
         data: JSON.stringify({myprice: myprice}),
         success: function (response) {
-            // 4. 모달을 종료한다. $('#container').removeClass('active');
+            // 모달 종료
             $('#container').removeClass('active');
-            // 5. 성공적으로 등록되었음을 알리는 alert를 띄운다.
             alert('성공적으로 등록되었습니다.');
-            // 6. 창을 새로고침한다. window.location.reload();
             window.location.reload();
         }
     })
